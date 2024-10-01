39be64c3481df0a1ea6e240b332572e915622346
Michael Roufa <roufamatic@gmail.com>
2024-09-30 22:06:46 -0700

    comments

--- src/main/java/frc/robot/commands/AprilTagRoundRobinFinder.java
+++ src/main/java/frc/robot/commands/AprilTagRoundRobinFinder.java
@@ -19,6 +19,8 @@ public class AprilTagRoundRobinFinder extends SequentialCommandGroup {
   private PhotonCamera m_camera;
   private NetworkTableInstance m_ntInstance;
   private int m_currentTag = 1;
+
+  // I bought four stands, so that's how many AprilTags we can find. :-)
   private static final int kMaxTag = 4;
 
   /**
@@ -30,26 +32,44 @@ public class AprilTagRoundRobinFinder extends SequentialCommandGroup {
    * @param drivetrain The drivetrain subsystem on which this command will run
    */
   public AprilTagRoundRobinFinder(Drivetrain drivetrain) {
+    // Can't go anywhere without a drivetrain!
     m_drivetrain = drivetrain;
+    // PhotonVision depends on NetworkTables. The robot code hosts the server,
+    // while the coprocessor has the client.
     m_ntInstance = NetworkTableInstance.getDefault();
     m_ntInstance.startServer();
     // "teenycam1" is a name I gave the camera using the PhotonVision web UI.
     m_camera = new PhotonCamera(m_ntInstance, "teenycam1");
+    // This is the tag number we are currently looking for.
     m_currentTag = 1;
 
+    // This command spins around until it finds the current tag.
     Command hunt = (new TurnDegrees(0.4, 360, m_drivetrain))
         .until(this::foundTag);
+    
+    // This command waits one second, then changes the current tag to the next
+    // highest number, or 1 if we're already at the maximum number.
     Command attack = (new WaitCommand(1))
         .andThen(Commands.runOnce(() -> {
             m_currentTag = m_currentTag == kMaxTag ? 1 : (m_currentTag + 1);
             System.out.printf("Staring at tag #%d", m_currentTag);
         }));
+    
+    // Finally we make one composite command that performs hunt / attack
+    // indefinitely.
     Command start = Commands.sequence(hunt, attack).repeatedly();
 
+    // AprilTagRoundRobinFinder is itself a SequentialCommand, so we can add
+    // even more commands here. I don't think they'd actually ever run because
+    // of the "repeatedly()" in the start command though!
     addCommands(start);
+
+    // Each command specifies what subsystems it requires.
     addRequirements(drivetrain);
   }
 
+  // This function checks every 20ms (the robot period) to see if the latest
+  // camera image contains the current AprilTag.
   private boolean foundTag() {
       PhotonPipelineResult cameraResult = m_camera.getLatestResult();
       if (!cameraResult.hasTargets()) {
